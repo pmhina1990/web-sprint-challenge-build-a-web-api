b4b5378cefd9c56cb0f01da3abfb15a522d30ee3
Peter Benjamin Mhina <peterbenjaminmhina@Peters-MacBook-Pro.local>
2021-06-04 11:40:42 -0700

    done

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const server = express();
-
+const actionRouter = require('./actions/actions-router')
+const projectsRouter = require('./projects/projects-router')
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+server.use(express.json())
 
+server.use('/api/actions', actionRouter)
+server.use('/api/projects', projectsRouter)
+
+server.get('/', (_, res) => {
+    res.send(`<h1>HAPPY TO BE HERE`)
+})
 module.exports = server;
